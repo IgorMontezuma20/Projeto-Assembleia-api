543dd2efe26204ea62c29fa89e675390b9045705
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-15 21:41:53 -0300

    feat: criando a entidade de pauta

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,7 +5,7 @@ export const databaseProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
-        type: 'mssql',
+        type: 'mysql',
         host: 'localhost',
         port: 3306,
         username: 'root',
