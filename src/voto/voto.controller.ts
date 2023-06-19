3e1b24fde013987468d93b54628c0e4798c5d2c1
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-18 22:14:19 -0300

    feat: fluxo simples de registro de voto implementado

--- src/voto/voto.controller.ts
+++ src/voto/voto.controller.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 import { PautasService } from 'src/pautas/pautas.service';
 import { VotoService } from './voto.service';
 import { ResgistroVotoDTO } from './voto.resource';
+import { ErrorResponse } from 'src/common/erro.resource';
 
 @Controller('pautas/:id/voto')
 export class VotoController {
@@ -15,8 +16,26 @@ export class VotoController {
   async registerVote(
     @Param('id') pautaId: string,
     @Body() registroVotoDTO: ResgistroVotoDTO,
-    @Res() Response: Response,
+    @Res() response: Response,
   ) {
-    return Response.status(HttpStatus.ACCEPTED).send();
+    const pauta = await this.pautasService.findById(pautaId);
+
+    if (!pauta) {
+      return response
+        .status(HttpStatus.NOT_FOUND)
+        .send(new ErrorResponse('Pauta não encontrada.'));
+    }
+
+    const result = await this.votosService.registerVote(
+      pauta,
+      registroVotoDTO.cpf,
+      registroVotoDTO.opcaoVoto,
+    );
+
+    //if (result.isError()) {
+    //  return response.status().send(new ErrorResponse(''));
+    //}
+
+    return response.status(HttpStatus.ACCEPTED).send();
   }
 }
