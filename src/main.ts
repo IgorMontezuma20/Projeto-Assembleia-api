74c3b04fe957ea1cc0a25f6d5813ed4ef6ea21f3
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-19 14:55:39 -0300

    feat: configurando o swagger da api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Assembleia API')
+    .setDescription('Api do projeto Assembleia.')
+    .setVersion('1.0.0')
+    .setContact(
+      'Igor Montezuma de Miranda',
+      'https://www.linkedin.com/in/igor-montezuma/',
+      'igorminranda17@gmail.com',
+    )
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDoc);
+
   await app.listen(3000);
 }
 bootstrap();
